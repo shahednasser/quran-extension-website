86bd687ae4c8ad66e5c42d831d0b95d47fed700a
shahed.nasser <shahed.nasser@lurner.com>
2021-01-21 13:12:58 +0200

    added amount input

--- js/donate.js
+++ js/donate.js
@@ -53,35 +53,32 @@ card.addEventListener('change', function(event) {
 // Handle form submission
 var form = document.getElementById('form-container');
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  tap.createToken(card).then(function(result) {
-    console.log(result);
-    if (result.error) {
-      // Inform the user if there was an error
-      var errorElement = document.getElementById('error-handler');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server
-      var errorElement = document.getElementById('success');
-      errorElement.style.display = "block";
-      var tokenElement = document.getElementById('token');
-      tokenElement.textContent = result.id;
-      tapTokenHandler(token)
+    //validate amount
 
-    }
-  });
+
+    tap.createToken(card).then(function(result) {
+        console.log(result);
+        if (result.error) {
+            // Inform the user if there was an error
+            errorElement.textContent = result.error.message;
+        } else {
+            // Send the token to your server
+            var errorElement = document.getElementById('success');
+            errorElement.style.display = "block";
+            var tokenElement = document.getElementById('token');
+            tokenElement.textContent = result.id;
+            // Insert the token ID into the form so it gets submitted to the server
+            var form = document.getElementById('payment-form');
+            var hiddenInput = document.createElement('input');
+            hiddenInput.setAttribute('type', 'hidden');
+            hiddenInput.setAttribute('name', 'tapToken');
+            hiddenInput.setAttribute('value', token.id);
+            form.appendChild(hiddenInput);
+
+            // Submit the form
+            //form.submit();
+        }
+    });
 });
-
-function tapTokenHandler(token) {
-    // Insert the token ID into the form so it gets submitted to the server
-    var form = document.getElementById('payment-form');
-    var hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'tapToken');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
-
-    // Submit the form
-    form.submit();
-}
