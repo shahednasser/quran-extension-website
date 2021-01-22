01a552fc276ea3a46356dde2d773b97db2aca19b
shahed.nasser <shahed.nasser@lurner.com>
2021-01-22 12:16:02 +0200

    removed donate form

--- js/main.js
+++ js/main.js
@@ -37,42 +37,6 @@ $(window).on('load', function() {
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
 
-	
-	/*------------------
-		Review
-	--------------------*/
-	var review_meta = $(".review-meta-slider");
-    var review_text = $(".review-text-slider");
-
-
-    review_text.on('changed.owl.carousel', function(event) {
-		review_meta.trigger('next.owl.carousel');
-	});
-
-	review_meta.owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 3,
-		center: true,
-		margin: 20,
-		autoplay: true,
-		mouseDrag: false,
-	});
-
-
-	review_text.owlCarousel({
-		loop: true,
-		nav: true,
-		dots: false,
-		items: 1,
-		margin: 20,
-		autoplay: true,
-		navText: ['<i class="ti-angle-left"><i>', '<i class="ti-angle-right"><i>'],
-		animateOut: 'fadeOutDown',
-    	animateIn: 'fadeInDown',
-	});
-
 
 
 	 /*------------------
